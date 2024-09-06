b7e80b9ca84343445234b4e77e604c60863107e5
Yimin <eamonsun@163.com>
2024-09-06 12:18:25 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,4 +15,9 @@
 from setuptools import setup
 
 
-setup(version="2.8.1")
+setup(name='paddleocr',
+    packages=['paddleocr'],
+    package_dir={'paddleocr': ''},
+    include_package_data=True,
+    entry_points={"console_scripts": ["paddleocr= paddleocr.paddleocr:main"]},
+      version='2.8.1')
